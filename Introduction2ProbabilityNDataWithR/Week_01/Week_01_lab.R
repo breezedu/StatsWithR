5c3f4ae6c88872b4bc060396168a1d1ad69dc523
jeff du <breeze.du@gmail.com>
2021-08-24 09:40:33 -0400

    update a new folder

---
+++ Introduction2ProbabilityNDataWithR/Week_01/Week_01_lab.R
@@ -0,0 +1,18 @@
+
+
+## Statistics with R
+## 
+## Part 01 Introduction to Probablity and Data with R
+## 
+## week 01 
+## install.packages("devtools")
+library(devtools) 
+
+
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("shiny")
+install_github("StatsWithR/statsr")
+
+## choose 1 to install/update ALL packages
+
